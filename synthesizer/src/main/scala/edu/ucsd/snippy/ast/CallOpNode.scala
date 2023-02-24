cb4af7b6afbf226eca5385d31fe1c50509fc5dc0
nick <nnebel@ucsd.edu>
2023-02-23 20:53:40 -0800

    fixed compiler errors

--- synthesizer/src/main/scala/edu/ucsd/snippy/ast/CallOpNode.scala
+++ synthesizer/src/main/scala/edu/ucsd/snippy/ast/CallOpNode.scala
@@ -4,37 +4,34 @@ import edu.ucsd.snippy.DebugPrints
 import edu.ucsd.snippy.ast.Types.Types
 import edu.ucsd.snippy.enumeration.Contexts
 
-class Call extends ASTNode
+case class Call(name: String, args: List[ASTNode]) extends ASTNode
 {
-	val name: String
-    val args: List[ASTNode]
-
 	override val height: Int = 1 + args.map(a=> a.height).max
 	override val terms: Int = 1 + args.foldLeft(0)((acc, arg) => acc + arg.terms)
 	override val children: Iterable[ASTNode] = args
 	override lazy val usesVariables: Boolean = args.foldLeft(false)((acc, arg) => acc || arg.usesVariables)
 	override protected val parenless: Boolean = false
+	override val nodeType: Types = Types.Any
+	override lazy val code: String = args.map(a => a.code).mkString(name + "(", ", ", ")")
 
 	// TODO: rewrite this assertion
 	//if (lhs.values.length != rhs.values.length) println(lhs.code, lhs.values, rhs.code, rhs.values)
 	//assert(lhs.values.length == rhs.values.length)
 
 	// TODO: implement
-	def doOp(args: List[Any]): Option[Any]  //{
+	def doOp(args: List[Any]): Option[Any] = None  //{
 		//case (l: Int, r: Int) => Some(l + r)
 		//case _ => wrongType(l, r)
 	//}
-	
-	def make(as: List[ASTNode]): Call = 
-		LessThanEq(as)
 
-	override val values: List[Option[Any]] = args.map(a => a.values)
-		.toArray.transpose.map(argValues => 
-			if (argValues.flatten[Any].length == argValues.length) 
-				this.doOp(left, right)
+	override val values: List[Option[Any]] = args.map(a => a.values.toArray)
+		.toArray.transpose.toList.map(maybeArgValues => {
+			val argValues = maybeArgValues.toList.flatten[Any]
+			if (argValues.length == maybeArgValues.length) 
+				this.doOp(argValues)
 			else 
 				None
-		)
+		})
 
 	def includes(varName: String): Boolean = args.foldLeft(false)((acc, arg) => 
 		acc || arg.includes(varName)
@@ -46,7 +43,5 @@ class Call extends ASTNode
 		None
 	}
 
-	override lazy val code: String = args.map(a => a.code).mkString(name + "(", ", ", ")")
-
-	override def updateValues(contexts: Contexts): Call = copy(args.map(a => a.updateValues(contexts)))
+	override def updateValues(contexts: Contexts): Call = copy(name, args.map(a => a.updateValues(contexts)).toList)
 }
